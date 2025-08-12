0037fa253c6adb4eb5b88809e5e65f1d4af361ab
imerrill9 <ian.merrill9@gmail.com>
2025-08-11 19:18:37 -0600

    Push ETS FeatureCollection snapshot on join

--- lib/wildfires_ws_web/channels/fires_channel.ex
+++ lib/wildfires_ws_web/channels/fires_channel.ex
@@ -2,10 +2,13 @@ defmodule WildfiresWsWeb.FiresChannel do
   use WildfiresWsWeb, :channel
 
   alias WildfiresWs.GeoJSON
+  alias WildfiresWs.IncidentsStore
 
   @impl true
   def join("fires:incidents", _payload, socket) do
-    snapshot = GeoJSON.empty_feature_collection()
+    snapshot =
+      IncidentsStore.get_all()
+      |> GeoJSON.feature_collection()
 
     push(socket, "snapshot", snapshot)
     {:ok, socket}
